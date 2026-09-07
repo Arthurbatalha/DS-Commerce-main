631038cb8cd4eeea56895c132cebdae7b78666ea
Arthurbatalha <133168271+Arthurbatalha@users.noreply.github.com>
2026-09-07 09:11:14 -0300

    fix Category

--- DS-Commerce-main/src/main/java/com/ArthurBatalha/dscommerce/entities/Category.java
+++ DS-Commerce-main/src/main/java/com/ArthurBatalha/dscommerce/entities/Category.java
@@ -25,6 +25,26 @@ public class Category {
 		this.name = name;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
